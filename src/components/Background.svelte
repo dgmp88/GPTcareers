1cd2ae338c737a28011e6a6c23b3b73df8064824
George Prichard <dgmprichard@gmail.com>
2023-02-12 13:20:16 +0000

    Background component

---
+++ src/components/Background.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import image from '$lib/images/matt-duncan-IUY_3DvM__w-unsplash.jpg';
+</script>
+
+<div
+	class="w-screen h-screen bg-cover bg-center bg-fixed -z-10 fixed blur"
+	style={`background-image: url(${image})`}
+/>
