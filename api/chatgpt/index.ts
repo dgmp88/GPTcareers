4f7b722bad650346a6497f80d5c703ec63764ab1
George Prichard <dgmprichard@gmail.com>
2023-07-12 15:14:03 +0100

    Vercel api endpoint added

---
+++ api/chatgpt/index.ts
@@ -0,0 +1,51 @@
+export const config = {
+  runtime: "edge",
+};
+
+const url = "https://api.openai.com/v1/chat/completions";
+
+function buildResponse(body: any) {
+  const response = new Response(body);
+  response.headers.set(
+    "Access-Control-Allow-Origin",
+    "https://thecareerfinder.app/"
+  );
+  response.headers.set("Access-Control-Allow-Credentials", "true");
+  response.headers.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, Content-Type, Accept"
+  );
+
+  response.headers.set("Content-Type", "text/event-stream");
+  response.headers.set("Cache-Control", "no-cache");
+  response.headers.set("Connection", "keep-alive");
+  response.headers.set("Content-Encoding", "none");
+  return response;
+}
+
+export default async function post(request: Request) {
+  if (request.method === "OPTIONS") {
+    return buildResponse("ok");
+  }
+
+  const { messages } = await request.json();
+
+  const body = {
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages,
+  };
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify(body),
+  };
+  const chatReq = await fetch(url, options);
+
+  return buildResponse(chatReq.body);
+}
